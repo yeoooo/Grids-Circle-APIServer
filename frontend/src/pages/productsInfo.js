5896e889b8e97192c699af8d7c8d8d8d8d3d69e5
yeoooo <ruisya330@gmail.com>
2022-10-01 20:37:20 +0900

    activate api for product Information

--- frontend/src/pages/productsInfo.js
+++ frontend/src/pages/productsInfo.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import p3 from '../assets/img/products-03.jpg'
-import p2 from '../assets/img/products-02.jpg'
-import p1 from '../assets/img/products-01.jpg'
 import {useEffect, useState} from "react";
 import axios from "axios";
 
@@ -26,25 +23,16 @@ export function ProductInfo(productInfo=[]) {
                                     <div className="product-item-title d-flex">
                                         <div className="bg-faded p-5 d-flex ms-auto rounded">
                                             <h2 className="section-heading mb-0">
-                                                {/*<span className="section-heading-upper">Blended to Perfection</span>*/}
                                                 <span className="section-heading-upper">{v.category}</span>
                                                 <span className="section-heading-lower">{v.productName}</span>
                                             </h2>
                                         </div>
                                     </div>
-                                    {/*<img className="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-5" src={p1} alt="..." />*/}
-                                    <div className="product-item-description d-flex me-auto">
+                                    <div className="product-item-description me-auto">
                                         <div className="bg-faded p-5 rounded">
-                                            <p className="mb-0">We take pride in our work, and it shows. Every time you
-                                                order a
-                                                beverage from us, we guarantee that it will be an experience worth having.
-                                                Whether it's our world famous Venezuelan Cappuccino, a refreshing iced
-                                                herbal
-                                                tea, or something as simple as a cup of speciality sourced black coffee, you
-                                                will be coming back for more.</p>
-                                            {/*<p>*/}
-                                            {/*    {v.description}*/}
-                                            {/*</p>*/}
+                                            <p>
+                                                {v.description}
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -58,28 +46,18 @@ export function ProductInfo(productInfo=[]) {
                             <div className="container">
                                 <div className="product-item">
                                     <div className="product-item-title d-flex">
-                                        <div className="bg-faded p-5 d-flex me-auto rounded">
+                                        <div className="bg-faded p-5 me-auto d-flex rounded">
                                             <h2 className="section-heading mb-0">
-                                                {/*<span className="section-heading-upper">Delicious Treats, Good Eats</span>*/}
-                                                {/*<span className="section-heading-lower">Bakery & Kitchen</span>*/}
                                                 <span className="section-heading-upper">{v.category}</span>
                                                 <span className="section-heading-lower">{v.productName}</span>
                                             </h2>
                                         </div>
                                     </div>
-                                    {/*<img className="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={p2} alt="..." />*/}
-                                    <div className="product-item-description d-flex ms-auto">
+                                    <div className="product-item-description ms-auto">
                                         <div className="bg-faded p-5 rounded">
-                                            <p className="mb-0">
-                                                Our seasonal menu features delicious snacks, baked goods, and even full
-                                                meals
-                                                perfect for breakfast or lunchtime. We source our ingredients from
-                                                local, oragnic
-                                                farms whenever possible, alongside premium vendors for specialty goods.
+                                            <p>
+                                                {v.description}
                                             </p>
-                                            {/*<p>*/}
-                                            {/*    {v.description}*/}
-                                            {/*</p>*/}
                                         </div>
                                     </div>
                                 </div>
