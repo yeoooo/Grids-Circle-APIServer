1acc254fc7c918dad34ddc622daed5b3e22905a3
yeoooo <ruisya330@gmail.com>
2022-10-07 12:07:28 +0900

    Test: Preadjust before docs

--- backend/src/test/java/com/example/gccoffee/GcCoffeeApplicationTests.java
+++ backend/src/test/java/com/example/gccoffee/GcCoffeeApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.gccoffee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.security.RunAs;
 
 @SpringBootTest
 @EnableJpaAuditing
--- backend/src/test/java/com/example/gccoffee/Service/OrderServiceImplTest.java
+++ backend/src/test/java/com/example/gccoffee/Service/OrderServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.annotation.Rollback;
